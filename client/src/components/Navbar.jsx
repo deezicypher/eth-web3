8daee9af01278be9ed2853e08d99f5f3a47bfa5e
deezitheviper <amuhdavid@gmail.com>
2023-02-27 01:54:33 +0100

    welcome and shii

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
-import logo from "../assets/images/logo.png"
+import logo from "../assets/images/xpeer.png"
 import { useState } from 'react'
 
 const NavbarItems = ({title,classProps}) => {
@@ -16,7 +16,7 @@ const Navbar = () => {
   return (
    <div className='w-full flex md:justify-center flex-row justify-between items-center p-4'>
       <div className='md:flex-[0.5] flex-initial justify-center items-center'>
-          <img  src={logo} alt="logo" className="w-32 cursor-pointer"/>
+          <img  src={logo} alt="logo" className="w-10 cursor-pointer"/>
       </div>
       <ul className="text-white md:flex hidden justify-between items-center  flex-initial">
          {
@@ -24,7 +24,7 @@ const Navbar = () => {
             <NavbarItems key={item + index} title={item} />
           ))
          }
-         <li className='bg-[#296350] py-2 px-7 mx-4 rounded-full cursor-pointer hover-[#25423d]'>
+         <li className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover-[#25423d]'>
           Login
          </li>
       </ul>
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import {AiFillPlayCircle} from "react-icons/ai";
+import {FaEthereum} from "react-icons/fa";
+import {BsFillInfoCircleFill} from "react-icons/bs";
+import {Loader} from "./";
+
 
 const Welcome = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-white">Welcome</h1>
+    <div className="flex w-full justify-center items-center">
+      <div className="flex md:flex-row flex-col justify-between items-start md:p-20 py-12 px-4">
+          <div className="flex flex-1 justify-start flex-col md:mr-10">
+            <h1 className=" text-3xl sm:text-5xl text-gradient py-1">
+                Peer to Peer Crypto <br/>
+                Accross the World
+            </h1>
+            <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
+               Send crypto to your family and friends easily on xpeer
+            </p>
+            <button type="button" onClick={() => connectWallet()} className="flex felx-row justify-center items-center my-5 cursor-pointer bg-[#2952e3] p-3 rounded-full hover:bg-[#2546bd] text-white">Connect</button>
+          </div>
+      </div>
+    </div>
   )
 }
 
