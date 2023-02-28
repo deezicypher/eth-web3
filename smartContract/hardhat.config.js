7787567eaa679fc7ba6da37ef02a2046648d0484
deezitheviper <amuhdavid@gmail.com>
2023-02-28 22:33:54 +0100

    tx deployment working

--- smartContract/hardhat.config.js
+++ smartContract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+//https://eth-goerli.g.alchemy.com/v2/vGy_5Jp2lltbZdwd6zesQktwFyUvTd2C
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('@nomiclabs/hardhat-waffle');
+
 module.exports = {
-  solidity: "0.8.17",
-};
+  solidity: '0.8.0',
+  networks: {
+    goerli:{
+      url:"https://eth-goerli.g.alchemy.com/v2/vGy_5Jp2lltbZdwd6zesQktwFyUvTd2C",
+      accounts: ['5ea360f7433c72f5ae00f95795c7ccbfc205c2f22eeb72709a1ac240d39feadb']
+    }
+  }
+}
